e9b8f179a4b5ea9b72dc43548f78d23a4954a924
Atule-Terlumun <130157725+Atule-Terlumun@users.noreply.github.com>
2023-04-15 21:47:21 +0100

    commit

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,72 +1,256 @@
-@extends('web.layout.layouts')
-@section('content')
 
-<!-- About Us -->
-<section class="py-5">
-    <div class="container ml-3 mr-3">
-        <div class="row">
-            <div class="col-lg-7 col-sm-12 col-xs-12">
-                <div class="row">
-                    <div class="col-lg-12">
-                        <h2 class="text-uppercase text-black">About High-edu<span class="text-success">
-                                Courses</span></h2>
-                    </div>
-                </div>
-                <div class="about-sev-tag mt-4">
-                    <h4 class="text-success">A little about us and a breif history of how we started.</h4>
-                    <p>Education is the passport to the future, for tomorrow belongs
-                        to those who prepare for it today.</p>
-                    <ul>
-                        <li class="check"><i class="fa fa-check-square bg-success" aria-hidden="true"></i> Educating
-                            the Society</li>
-                        <li class="check"><i class="fa fa-check-square bg-success" aria-hidden="true"></i> Training
-                            the leaders of tomorrow</li>
-                        <li class="check"><i class="fa fa-check-square bg-success" aria-hidden="true"></i> Anyone
-                            who loves or pursues or desires</li>
-                        <li class="check"><i class="fa fa-check-square bg-success" aria-hidden="true"></i> The
-                            printing and typesetting industry.</li>
+                <!-- HOME PAGE -->
+                <div class="bg-white">
+                    <div id="demo" class="carousel slide" data-ride="carousel">
+
+                    <!-- Indicators -->
+                    <ul class="carousel-indicators">
+                        <li data-target="#demo" data-slide-to="0" class="active"></li>
+                        <li data-target="#demo" data-slide-to="1"></li>
+                        <li data-target="#demo" data-slide-to="2"></li>
                     </ul>
 
-                    <div class="founder-text">
-                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum
-                            has been the industry's</p>
-                        <h3>- Jimmy Dev.</h3>
+                    <!-- The slideshow -->
+                            <div class="carousel-inner">
+                                <div class="carousel-item active">
+                                    <img src="{{asset('images/peters.jpg')}}" class="images d-block w-100" style="height:500px;" alt="Los Angeles">
+                                        <div class="carousel-content">
+                                            <h1>PROUDLY, ADVANZTEK TO THE WORLD</h1> <br>
+                                            <h4>AM HAPPY FOR THIS SCHOLARSHIP OFFER</h4>
+                                        </div>
+                                    <div class="carousel-caption" >
+                                        <h1>Jacob Tarkumbur Oraduen</h1>
+                                    </div>
+                                </div>
+                            <div class="item">
+
+                        </div>
+                        <div class="carousel-item">
+                            <img src="{{asset('images/academia.jpg')}}" class="images d-block w-100" style="height:500px;" alt="Chicago">
+                            <div class="carousel-content">
+                                            <h1>PROUDLY, ADVANZTEK TO THE WORLD</h1> <br>
+                                            <h4>AM HAPPY FOR THIS SCHOLARSHIP OFFER</h4>
+                                        </div>
+                                    <div class="carousel-caption" >
+                                        <h1>Elder Usue Terkula Gabriel</h1>
+                                    </div>
+                                </div>
+                            <div class="item">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="{{asset('images/ladycomputer.jpg')}}" class="images d-block w-100" style="height:500px;"  alt="New York">
+                            <div class="carousel-content">
+                                            <h1>PROUDLY, ADVANZTEK TO THE WORLD</h1> <br>
+                                            <h4>AM HAPPY FOR THIS SCHOLARSHIP OFFER</h4>
+                                        </div>
+                                    <div class="carousel-caption" >
+                                        <h1>Comrade Peter Terlumun Atule</h1>
+                                    </div>
+                                </div>
+                            <div class="item">
+                        </div>
+                    </div>
+
+                    <!-- Left and right controls -->
+                    <a class="carousel-control-prev" href="#demo" data-slide="prev">
+                        <span class="carousel-control-prev-icon"></span>
+                    </a>
+                    <a class="carousel-control-next" href="#demo" data-slide="next">
+                        <span class="carousel-control-next-icon"></span>
+                    </a>
+
+            </div>
+            <div class="">
+            <div class="why Educare">
+                    <div class="from-the-best">
+                    <div class="container py-2">
+                        <div class="text-center">
+                            <h2 class="text-uppercase text-black">why chose <span class="text-success">
+                                Educare</span>
+                            </h2>
+                                <div class="lines mb-5">
+                              
+                                </div>
+                        </div>
+                    <div class="text-center"> 
+                    <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa temporibus corrupti nesciunt <br>
+                     odit necessitatibus maxime aspernatur ab aut, tempora quos, recusandae aliquid <br>
+                      molestiae excepturi impedit sed atque unde quas consectetur.</p>   
+                    </div>
+                    <div class="">
+                        
+                    </div>
+                <div class="row">
+                    <div class="col-lg-5 col-md-7 col-sm-12">
+                            <div class="">
+                                <img src="{{asset('images/main-feature.png')}}" alt="">
+                            </div>
+                    </div>
+                    <div class="col-lg-6 col-md-6 col-sm-12">
+                            <div class="row">
+                                <div class="col-3">
+                                    <i class="fa-solid fa-facebook-square"></i>
+                                </div>
+                                <div class="col-9">
+                                    <h4>RESIDENTIAL SERVICES</h4>
+                                        <p>Backed by some of the biggest names in the industry,
+                                     Firefox OS is an open platform that fosters greater</p>
+                                </div>
+                                <div class="col-3">
+                                    <i class="fas fa-shopping-cart"></i>
+                                </div>
+                                <div class="col-9">
+                                    <h4>COMMERCIAL SERVICES</h4>
+                                        <p>Backed by some of the biggest names in the industry, 
+                                     Firefox OS is an open platform that fosters greater</p>
+                                </div>
+                                <div class="col-3">
+                                    <i class="fas fa-shopping-cart"></i>
+                                </div>
+                                <div class="col-9">
+                                    <h4>OUR BEST STAFF</h4>
+                                        <p>Backed by some of the biggest names in the industry, 
+                                     Firefox OS is an open platform that fosters greater</p>
+                                </div>
+                                <div class="col-3">
+                                    <i class="fas fa-shopping-cart"></i>
+                               </div>
+                                <div class="col-9">
+                                    <h4>24 HOURS SERVICES</h4>
+                                        <p>Backed by some of the biggest names in the industry,
+                                     Firefox OS is an open platform that fosters greater</p>
+                                </div>
+                            </div>
+                    </div>
+                </div>      
+            </div>
+
+
+                <!-- PHOTO GALLERY -->
+                <div class="bg-light">
+                    <div class="container py-5">
+                        <div class="text-center">
+                            <h2 class="text-uppercase text-black">photo <span class="text-success">
+                                Gallery</span>
+                            </h2>
+                                <div class="lines mb-5">
+                                   
+                                </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-lg-3">
+                                <div class="card mb-4">
+                                    <img src="{{asset('images/1.jpg')}}" alt="" srcset="">
+                                </div>
+                            </div>
+                            <div class="col-lg-3">
+                                <div class="card">
+                                    <img src="{{asset('images/2.jpg')}}" alt="" srcset="">
+                                </div>
+                            </div>
+                            <div class="col-lg-3">
+                                <div class="card">
+                                    <img src="{{asset('images/3.jpg')}}" alt="" srcset="">
+                                </div>
+                            </div>
+                            <div class="col-lg-3">
+                                <div class="card">
+                                    <img src="{{asset('images/4.jpg')}}" alt="" srcset="">
+                                </div>
+                            </div>
+                            <div class="col-lg-3">
+                                <div class="card">
+                                    <img src="{{asset('images/5.jpg')}}" alt="" srcset="">
+                                </div>
+                            </div>
+                            <div class="col-lg-3">
+                                <div class="card">
+                                    <img src="{{asset('images/6.jpg')}}" alt="" srcset="">
+                                </div>
+                            </div>
+                            <div class="col-lg-3">
+                                <div class="card">
+                                    <img src="{{asset('images/7.jpg')}}" alt="" srcset="">
+                                </div>
+                            </div>
+                            <div class="col-lg-3">
+                                <div class="card">
+                                    <img src="{{asset('images/8.jpg')}}" alt="" srcset="">
+                                </div>
+                            </div>
+                        </div>
+                    </div>    
+                </div>
+                </div>
+                <div class="we-are">
+                    <div class="container py-5">
+                    <div class="col-lg-6 col-md-6 col-sm-12">
+                        <div class="">
+                            <h2>We Are With You Every Step <br>
+                                Get Started Easily. </h2><br>
+                               <span>Make your Website Faster, Safer & Better Support.</span> 
+                            
+                        </div>
+                        <div >
+                            <button class="btn btn-success btn-piled">GET STARTED NOW!</button> 
+                        </div>
+                    </div>
                     </div>
                 </div>
+                
             </div>
-            <div class="col-lg-5 col-sm-12 col-xs-12">
-                <div class="about-sev-img"> <img src="{{ asset('assets/images/teee.jpg') }}" alt="image" height="410px"
-                        width="500px"> </div>
-            </div>
-        </div>
+
+
+<!-- templete -->
+<section id="main-features" class="padding gray">
+  <div class="container">
+    <div class="row mb-20">
+      <div class="col-sm-1 col-md-2"></div>
+      <div class="col-xs-12 col-sm-10 col-lg-8 text-center">
+        <h2 class="text-uppercase">Why choose <span class="color_red">EduCare</span></h2>
+        <div class="line_1-1"></div>
+        <div class="line_2-2"></div>
+        <div class="line_3-3"></div>
+        <p class="heading_space">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+      </div>
+      <div class="col-sm-1 col-lg-2"></div>
     </div>
+    <div class="row">
+      <div class="col-sm-5">
+        <div class="features-img"> <img src="./main-feature.png" alt=""> </div>
+      </div>
+      <div class="col-sm-7">
+        <div class="media service-box">
+          <div class="pull-left"> <i class="fa fa-cloud-download" aria-hidden="true"></i> </div>
+          <div class="media-body">
+            <h4 class="media-heading">Residential Services</h4>
+            <p>Backed by some of the biggest names in the industry, Firefox OS is an open platform that fosters greater</p>
+          </div>
+        </div>
+        <div class="media service-box">
+          <div class="pull-left"> <i class="fa fa-paper-plane-o" aria-hidden="true"></i></div>
+          <div class="media-body">
+            <h4 class="media-heading">Commercial Services</h4>
+            <p>Backed by some of the biggest names in the industry, Firefox OS is an open platform that fosters greater</p>
+          </div>
+        </div>
+        <div class="media service-box">
+          <div class="pull-left"><i class="fa fa-balance-scale" aria-hidden="true"></i> </div>
+          <div class="media-body">
+            <h4 class="media-heading">Our Best Staff</h4>
+            <p>Backed by some of the biggest names in the industry, Firefox OS is an open platform that fosters greater</p>
+          </div>
+        </div>
+        <div class="media service-box">
+          <div class="pull-left"> <i class="fa fa-bullhorn" aria-hidden="true"></i> </div>
+          <div class="media-body">
+            <h4 class="media-heading">24 Hours Services</h4>
+            <p>Backed by some of the biggest names in the industry, Firefox OS is an open platform that fosters greater</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </section>
 
-<!-- Get Stated Now -->
-<div class=" px-5" style="background-color: rgb(0, 137, 55);">
-    <div class="row w-100 py-4">
-        <div class="col-lg-6 ">
-            <ul class="">
-                <li class="d-block mt-2">
-                    <a class="text-white" href="#"><i class=" mb-0"></i>
-                        <h2>Lorem ipsum dolor sit amet</h2>
-                    </a>
-                    <a class="text-white" href="#"><i class=" mt-0">
-                            <h6>Lorem ipsum dolor sit amet, consectetur</h6>
-                        </i></a>
-                </li>
-            </ul>
-        </div>
-        <div class="col-lg-6 items py-3">
-            <ul class="d-flex justify-content-end">
-                <li class="d-block mt-2 align-items-end">
-                    <button class="btn btn-outline-white bg-white text-black" name="btn_get_started">GET STARTED
-                        NOW</button>
-                </li>
-            </ul>
-        </div>
-    </div>
-</div>
 
-
-@endsection

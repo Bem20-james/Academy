30a6777ecb759324397b951e9270a7ce5c2dead8
tarkumburjacob <130210253+tarkumburjacob@users.noreply.github.com>
2023-04-18 02:15:06 +0100

    work done

--- resources/views/web/includes/header.blade.php
+++ resources/views/web/includes/header.blade.php
@@ -1,7 +1,7 @@
 <!-- Head -->
-<div class="top-head">
-    <div class="row w-100">
-        <div class="col-lg-6 pt-1 pb-0">
+<div class="top-head pb-0">
+    <div class="row w-100 py-0 px-0">
+        <div class="col-lg-6 pt-2">
             <ul class="">
                 <li class="d-block">
                     <a class="items " href="#">
@@ -10,17 +10,17 @@
                 </li>
             </ul>
         </div>
-        <div class="col-lg-6 pt-1 register">
+        <div class="col-lg-6 pt-2">
             <ul class="d-flex justify-content-end register">
                 <li class="d-block align-items-end">
-                    <a class="items " href="#"><i class="mr-2"></i>Register</a>
+                    <a class="items " href="#"><i class="mr-3"></i>Register</a>
                     |
-                    <a class="items mr-3" href="#"><i class="mr-2"></i>Login</a>
+                    <a class="items mr-3" href="#"><i class="mr-1"></i>Login</a>
                     <a class="items" href="#"><i class="fa-brands fa-facebook ml-5 mr-4 "></i></a>
                     <a class="items" href="#"><i class="fa-brands fa-whatsapp mr-4 "></i></a>
                     <a class="items" href="#"><i class="fa-brands fa-twitter mr-4 "></i></a>
                     <a class="items" href="#"><i class="fa-brands fa-instagram mr-4 "></i></a>
-                    <a class="items" href="#"><i class="fa-brands fa-google-plus mr-4 "></i></a>
+                    <a class="items" href="#"><i class="fa-brands fa-google-plus mr-3 "></i></a>
                 </li>
             </ul>
         </div>
@@ -28,32 +28,31 @@
 </div>
 
 <!-- Main-Hearder -->
-<nav class="navbar navbar-expand-lg navbar-light" style="background-color: #154F4F;">
-    <img src="assets/images\Logo Academia.png" hight="70px" width="80px" alt="Academia Logo">
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-
-    <div class="collapse navbar-collapse  " id="navbarSupportedContent">
+<nav class="navbar navbar-expand-lg navbar-light p-0 m-0 d-flex" style="background-color: #154F4F;">
+    <div class="container">
+        <img src="assets/images\Logo Academia.png" width="80px" height="auto" alt="Academia Logo">
+        <button class="navbar-toggler p-0 m-0" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
         <ul class="navbar-nav justify ml-auto homepage-links">
-            <li class="nav-item active">
-                <a class="nav-link text-white mr-5 ml-1" href="#">Home<span class="sr-only">(current)</span></a>
+            <li class="nav-item active p-0 m-0">
+                <a class="nav-link text-white p-4 mx-2 h-100" href="#">Home<span class="sr-only">(current)</span></a>
             </li>
             <li class="nav-item">
-                <a class="nav-link text-white mr-5 ml-1" href="#">About Us</a>
+                <a class="nav-link text-white p-4 mx-2" href="#">About Us</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link text-white mr-5 ml-1" href="#">Courses</a>
+                <a class="nav-link text-white p-4 mx-2" href="#">Courses</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link text-white mr-5 ml-1" href="#">Contact</a>
+                <a class="nav-link text-white p-4 mx-2" href="#">Contact</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link text-white mr-5 ml-1" href="#">Blog</a>
+                <a class="nav-link text-white p-4 mx-2" href="#">Blog</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link text-white mr-5 ml-1" href="#">Gallery</a>
+                <a class="nav-link text-white p-4 mx-2" href="#">Gallery</a>
             </li>
         </ul>
         <form class="form-inline my-2 my-lg-0 ">
